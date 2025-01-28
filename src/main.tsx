e503d887ddb05afd2bfcfe0af7c1a25435f5025a
nikogalvan <ramiro_coronel333@hotmail.com>
2025-01-28 17:06:13 -0300

    style: import CSS in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import App from './App'
+import './styles/normalize.css'
+import './styles/index.css'
+import './styles/variables.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
