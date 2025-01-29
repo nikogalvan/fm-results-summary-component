bda0b4ba2f0b004006b93e9cf844eb87ce84e567
nikogalvan <ramiro_coronel333@hotmail.com>
2025-01-29 13:34:01 -0300

    feat(CardSummary): add basic HTML structure

--- src/components/card-summary/CardSummary.tsx
+++ src/components/card-summary/CardSummary.tsx
@@ -1,9 +1,19 @@
+import { SUMMARY_INFO } from "../../constants/summary-info";
+
 const CardSummary = () => {
   return (
     <div>
       <h2>Summary</h2>
+      {SUMMARY_INFO.map((summaryItem) => (
+        <div key={summaryItem.id}>
+          <img src={summaryItem.icon} alt={summaryItem.category} />
+          <span>{summaryItem.category}</span>
+          <span>{summaryItem.score}</span>
+          <span>/ 100</span>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default CardSummary
+export default CardSummary;
