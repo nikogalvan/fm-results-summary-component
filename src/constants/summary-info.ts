47fd73dbdc9be20de7533d96c4051d6fde468f1a
nikogalvan <ramiro_coronel333@hotmail.com>
2025-01-29 13:31:44 -0300

    refactor(summary-info): migrate to TypeScript and add interface

---
+++ src/constants/summary-info.ts
@@ -0,0 +1,39 @@
+const PATH = "/assets/images/";
+
+interface SummaryItem {
+  id: string;
+  category: string;
+  score: number;
+  icon: string;
+  color: string;
+}
+export const SUMMARY_INFO: SummaryItem[] = [
+  {
+    id: crypto.randomUUID(),
+    category: "Reaction",
+    score: 80,
+    icon: `${PATH}icon-reaction.svg`,
+    color: "red",
+  },
+  {
+    id: crypto.randomUUID(),
+    category: "Memory",
+    score: 92,
+    icon: `${PATH}icon-memory.svg`,
+    color: "yellow",
+  },
+  {
+    id: crypto.randomUUID(),
+    category: "Verbal",
+    score: 61,
+    icon: `${PATH}icon-verbal.svg`,
+    color: "green",
+  },
+  {
+    id: crypto.randomUUID(),
+    category: "Visual",
+    score: 72,
+    icon: `${PATH}icon-visual.svg`,
+    color: "purple",
+  },
+];
