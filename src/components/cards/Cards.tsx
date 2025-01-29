ecdd3d54798954454bdcdd622807f39d28324488
nikogalvan <ramiro_coronel333@hotmail.com>
2025-01-29 12:45:47 -0300

    style: add styles to Cards component

--- src/components/cards/Cards.tsx
+++ src/components/cards/Cards.tsx
@@ -1,11 +1,13 @@
 import  { FC, ReactNode } from "react";
+import styles from './Cards.module.css';
 
 interface CardsProps {
   children: ReactNode;
 }
 
 const Cards: FC<CardsProps> = ({ children }) => {
-  return <div>{children}</div>;
+  const {cards} = styles;
+  return <div className={cards}>{children}</div>;
 };
 
 export default Cards;
